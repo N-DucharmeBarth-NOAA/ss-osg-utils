9882f879ccf8820aacf72459e09b3e65def4592e
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2023-01-17 09:13:53 -1000

    add roxygen tag for zzz

--- R/zzz.r
+++ R/zzz.r
@@ -1,6 +1,8 @@
+#' @importFrom utils packageVersion
+
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("###########################################################################################################")
-  packageStartupMessage("Loading package ssgrid version ", packageVersion("ssgrid") )
+  packageStartupMessage("Loading package ssgrid version ", utils::packageVersion("ssgrid") )
   packageStartupMessage("Use of ssgrid implies adherence to the OSG Connect and OSPool acceptable use policy.")
   packageStartupMessage("OSG Connect may only be used for work relevant to research and/or education with an affiliated institution.")
   packageStartupMessage("Data with any  privacy concerns should not be computed in OSG.")
